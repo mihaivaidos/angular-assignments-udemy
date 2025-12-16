d3251cc345372180b91081d931a3c345ba0dbe7f
mihaivaidos <mihai@vaidos.com>
2025-12-16 11:12:44 +0200

    Assignment 1 components and update AppComponent to use them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
+import {WarningAlertComponent} from './assignment-1/warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from './assignment-1/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/assignment-1/ASSIGNMENT-1.md
@@ -0,0 +1,6 @@
+<ol>
+  <li>Create two new Components (manually or with CLI): WarningAlert and SuccessAlert</li>
+  <li>Output them beneath each other in the AppComponent</li>
+  <li>Output a warning or success message in the Components</li>
+  <li>Style the Components appropriately (maybe some red/ green text?)</li>
+</ol>
