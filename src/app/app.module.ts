b3a8ad18acf1ab4dc50a7793971675f14f7c061d
mihaivaidos <mihai@vaidos.com>
2025-12-17 14:52:02 +0200

    Add UserReactiveFormComponent for Assignment 7 with form controls and custom validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {WarningAlertComponent} from './assignment-1/warning-alert/warning-alert.component';
 import {SuccessAlertComponent} from './assignment-1/success-alert/success-alert.component';
 import {UsernameComponent} from './assignment-2/username/username.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SecretComponent} from './assignment-3/secret/secret.component';
 import {GameControlComponent} from './assignment-4/game-control/game-control.component';
 import {OddComponent} from './assignment-4/odd/odd.component';
 import {EvenComponent} from './assignment-4/even/even.component';
 import {GameComponent} from './assignment-4/game/game.component';
 import {ActiveUsersComponent} from './assignment-5/active-users/active-users.component';
-import { InactiveUsersComponent } from './assignment-5/inactive-users/inactive-users.component';
-import { UsersComponent } from './assignment-5/users/users.component';
-import { UserFormComponent } from './assignment-6/user-form/user-form.component';
+import {InactiveUsersComponent} from './assignment-5/inactive-users/inactive-users.component';
+import {UsersComponent} from './assignment-5/users/users.component';
+import {UserFormComponent} from './assignment-6/user-form/user-form.component';
+import {UserReactiveFormComponent} from './assignment-7/user-reactive-form/user-reactive-form.component';
 
 @NgModule({
   declarations: [
@@ -29,9 +30,10 @@ import { UserFormComponent } from './assignment-6/user-form/user-form.component'
     ActiveUsersComponent,
     InactiveUsersComponent,
     UsersComponent,
-    UserFormComponent
+    UserFormComponent,
+    UserReactiveFormComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
